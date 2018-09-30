b6507e5fa4ce06479c2c8a8ecbb47d87564c26d6
mrisump <30776472+mrisump@users.noreply.github.com>
2018-09-30 14:54:29 +0200

    Added documentation to vec2d. Added a table to 4g2.fsx.

--- 4g2.fsx
+++ 4g2.fsx
@@ -1,3 +1,11 @@
+(* Opgave 4g.2
+ *
+ * Gruppe 10:
+ *  Jonas Friis
+ *  Claes Heise
+ *  Morten Risum Pedersen
+ *)
+
 /// Regardless, I have no idea what this function does.
 /// It should return some sort of polygon length.
 /// Really, it converts a float to an integer.
@@ -6,12 +14,26 @@
  * I barely know what this function is supposed to do.
  * It should probably take the sum of the length of the vectors.
  * The vectors are defined as the cos and sin of times (?) in a circle.
- * The vectors are then the length of the vectors between the neighbouring points.
+ * They are then the length of the vectors between the neighbouring points.
  * What the neighbouring points are has yet to be discovered.
  * Please send microscope and math.
  *)
 
-/// FUNCTION SHOULD TAKE AN INT, NOT A FLOAT!
+(* (a) *)
+
+///<summary>
+///Approximates the cirfumference of a circle with radius 1 with steps n.
+///</summary>
+///<params name="n">
+///The amount of vectors to use for calculation.
+///Larger n produce more accurate results.
+///</params>
+///<returns>
+///The length of the polygon as a float.
+///</returns>
+///<remarks>
+///DELETE IF NO REMARKS. /////////////////////////////////////////////////////!
+///</remarks>
 let polyLen (n : float) : float =
   let mutable i : float = 0.0
   let mutable s : float = 0.0
@@ -21,3 +43,21 @@ let polyLen (n : float) : float =
   s
 
 printfn "%g" (polyLen 48290.248932)
+
+///Should be included in the polyLen function.
+///Consider issues arising from taking variable n (for row lengt, not implm.).
+let lengthTable (n : int) : string =
+  let mutable strt = ""
+  strt <- strt + "\n   n polyLen circumference\n"
+  /// Row Control (modify to work with i = (n-10) to n)
+  for i = 1 to 10 do
+    /// Prints leftmost column
+    strt <- strt + sprintf "%4i" i
+    /// Column Control
+    strt <- strt + sprintf " %7g %13g" (3.14) (6.28)
+    strt <- strt + sprintf "\n"
+  sprintf "%s" strt
+
+printf "%s" (lengthTable 10)
+
+(* (b) *)
--- vec2d.fs
+++ vec2d.fs
@@ -8,25 +8,83 @@
 
 module vec2d
 
+///<summary>
+///Calculates the length of a vector.
+///</summary>
+///<params name="(x, y)">
+///A vector defined as a tuple (x, y).
+///</params>
+///<returns>
+///The length of the vector as a float.
+///</returns>
 let len ((x, y) : (float * float)) : float =
     ((x*x)+(y*y))**(1.0/2.0)
 
+///<summary>
+///Calculates the angle of a vector, relative to the vector (1,0).
+///</summary>
+///<params name="(x, y)">
+///A vector defined as a tuple (x, y).
+///</params>
+///<returns>
+///The angle of the vector, relative to the vector (1,0), as a float.
+///</returns>
 let ang ((x, y) : (float * float)) : float =
     (atan2 y x)
 
-/// The arguments were given incorrectly, per the signature file.
-///let scale ((x,y) : (float * float)) (a:float) : (float * float) =
-///    ((x*a),(y*a))
-
+///<summary>
+///Scales a vector by multiplying a float with the vector.
+///</summary>
+///<params name="a">
+///The factor to scale the vector by.
+///</params>
+///<params name="(x, y)">
+///A vector defined as a tuple (x, y).
+///</params>
+///<returns>
+///A scaled vector as a tuple.
+///</returns>
 let scale (a : float) ((x,y) : (float * float)) : (float * float) =
     ((x*a), (y*a))
 
+///<summary>
+///Adds two vectors.
+///</summary>
+///<params name="(x1, y1)">
+///The first vector, defined as a tuple (x, y).
+///</params>
+///<params name="(x2, y2)">
+///The second vector, defined as a tuple (x, y).
+///</params>
+///<returns>
+///The sum of the two vectors, defined as a tuple (x, y).
+///</returns>
+///<remarks>
+///Function takes up space greater than 80 characters.
+///</remarks>
 let add ((x1, y1) : (float * float)) ((x2, y2) : (float * float)) : (float * float) =
     (x1+x2, y1+y2)
+
+///<summary>
+///Calculates the dot product of two vectors.
+///</summary>
+///<params name="(x1, y1)">
+///The first vector defined as a tuple (x, y).
+///</params>
+///<params name="(x2, y2)">
+///The second vector defined as a tuple (x, y).
+///</params>
+///<returns>
+///The dot product of two vectors as a float.
+///</returns>
+let dot ((x1, y1) : (float * float)) ((x2, y2) : (float * float)) : float =
+    x1*x2+y1*y2
+
+///Scraps.
+///The arguments were given incorrectly, per the signature file.
+///let scale ((x,y) : (float * float)) (a:float) : (float * float) =
+///    ((x*a),(y*a))
 
 ///This produces the wrong result; a vector with two coordinates.
 ///let dot ((x1,y1) : (float * float)) ((x2,y2): (float * float)) : (float * float) =
 ///    ((x1*x2),(y1*y2))
-
-let dot ((x1, y1) : (float * float)) ((x2, y2) : (float * float)) : float =
-    x1*x2+y1*y2
